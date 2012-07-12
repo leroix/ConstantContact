d2dc14fddf9ec7cbde0055c0e9050c0304dc457d
Justin Ratner <leroix08@gmail.com>
2012-07-11 23:05:19 -0600

    package module

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='ConstantContact',
+    version='0.1.0',
+    author='Justin Ratner',
+    author_email='justin@rafflecopter.com',
+    packages=find_packages(),
+    url='https://github.com/leroix/ConstantContact',
+    license='LICENSE.txt',
+    description='Constant Contact api clent',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "oauth2 >= 1.5.211",
+    ],
+)
